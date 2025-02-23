ad88d7ff871d63d27651f929a96204257ee8ebec
Ryan Lau <47727459+ryanlau@users.noreply.github.com>
2025-02-23 02:47:04 -0500

    v12

--- www/app/layout.tsx
+++ www/app/layout.tsx
@@ -4,25 +4,24 @@ import { Public_Sans } from 'next/font/google'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+	title: 'v0 App',
+	description: "Experience our revolutionary card grading technology that's changing how card collectors assess, value, and trade their cards with confidence and transparency",
 }
 
 const public_sans = Public_Sans({
-  display: 'swap',
-  subsets: ['latin'],
-  variable: '--font-public-sans',
+	display: 'swap',
+	subsets: ['latin'],
+	variable: '--font-public-sans',
 })
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="en" className={`${public_sans.variable}`}>
-      <body>{children}</body>
-    </html>
-  )
+	return (
+		<html lang="en" className={`${public_sans.variable}`}>
+			<body>{children}</body>
+		</html>
+	)
 }
